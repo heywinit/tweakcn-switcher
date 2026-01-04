f1f38a6dc0cbfa3da3ca2ecc70d60679f76454fa
winit <heywinit@gmail.com>
2026-01-04 05:57:19 +0530

    starting from scratch

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { ThemeProvider, useTheme } from "./provider";
-export { ThemeRegistry } from "./registry";
-export { applyTheme, removeTheme, getSystemColorScheme } from "./applyTheme";
-export type { TweakcnTheme, ThemeProviderProps, UseThemeProps } from "./types";
